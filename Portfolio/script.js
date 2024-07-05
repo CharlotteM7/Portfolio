f6e86a46691b6d89f1617d93d2845759ab9d1c5c
Charlotte Mackay <77455208+CharlotteM7@users.noreply.github.com>
2024-07-05 13:25:05 +0100

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,22 @@
+        // Smooth Scrolling
+        document.querySelectorAll('nav ul li a').forEach(anchor => {
+            anchor.addEventListener('click', function(e) {
+                const href = this.getAttribute('href');
+                if (!href.startsWith('Docs/')) { // Only prevent default for internal links, not external ones
+                    e.preventDefault();
+                    document.querySelector(href).scrollIntoView({
+                        behavior: 'smooth'
+                    });
+                }
+            });
+        });
+
+
+
+        // Toggle Menu for Mobile Devices
+        document.querySelector('.menu-toggle').addEventListener('click', () => {
+            document.querySelector('nav ul').classList.toggle('active');
+        });
+
+        // Initialize AOS
+        AOS.init();
