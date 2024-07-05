fd670bb07e14ff3a25f9dd0acd92261baa5c092c
Charlotte Mackay <77455208+CharlotteM7@users.noreply.github.com>
2024-07-05 21:22:38 +0100

    Link updates

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
         document.querySelectorAll('nav ul li a').forEach(anchor => {
             anchor.addEventListener('click', function(e) {
                 const href = this.getAttribute('href');
-                if (!href.startsWith('Docs/')) { // Only prevent default for internal links, not external ones
+                if (!href.startsWith('Portfolio/')) { // Only prevent default for internal links, not external ones
                     e.preventDefault();
                     document.querySelector(href).scrollIntoView({
                         behavior: 'smooth'
